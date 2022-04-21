f9b5f2641261a4b5b7a37231c5f5f4a2330b3542
unknown <simeonolawale69@gmail.com>
2022-04-21 14:54:51 +0100

    >>>

---
+++ main.js
@@ -0,0 +1,6 @@
+var name = '30 days of javascript';
+var go ="challenge";
+console.log(name.toLowerCase());
+check.innerHTML=name.toUpperCase()
+console.log(name.substr(4, 17));
+check.innerHTML=name.substr(5, 17);
